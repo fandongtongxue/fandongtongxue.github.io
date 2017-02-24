548e2fa92b8a1dd17afcf2b5235cef5725db8ab2
fandongtongxue <admin@fandong.me>
2017-02-24 13:58:45 +0800

    删除无用东西

--- 报名_files/rem.js
+++
@@ -1,9 +0,0 @@
-function getfont() {
-	var html1 = document.documentElement;
-	var screen = html1.clientWidth;
-	html1.style.fontSize = 0.02666666666666667 * screen + 'px';
-};
-getfont();
-window.onresize = function() {
-	getfont();
-};
