059b544a40a22bdecfcbb6b8a1fad3bef2bf5965
fandong <fand@wisenjoy.com>
2017-08-28 13:55:28 +0800

    提交代码

--- sw.js
+++ sw.js
@@ -32,9 +32,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "blog.fandong.me"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
